06eb37fe7530897a252741b1a795aa1349ca4674
prastowoadi18 <prastowoadiwork@gmail.com>
2023-05-02 17:03:14 +0700

    change origin url

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({ origin: "https://mytimesheet-sigma.vercel.app", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
