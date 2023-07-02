9208a0ce45aebff636067c0691faa21ccbe6a42a
prastowoadi18 <prastowoadiworkv@gmail.com>
2023-07-02 23:59:11 +0700

    Add wedding api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,24 +1,23 @@
-import User from "../models/user.model.js";
 import Wedding from "../models/weddingform/weeding.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
-export const register = async (req, res, next) => {
-  try {
-    const hash = bcrypt.hashSync(req.body.password, 5);
+// export const register = async (req, res, next) => {
+//   try {
+//     const hash = bcrypt.hashSync(req.body.password, 5);
 
-    const newUser = new User({
-      ...req.body,
-      password: hash,
-    });
+//     const newUser = new User({
+//       ...req.body,
+//       password: hash,
+//     });
 
-    await newUser.save();
-    res.status(201).send("User has been created.");
-  } catch (err) {
-    next(err);
-  }
-};
+//     await newUser.save();
+//     res.status(201).send("User has been created.");
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 export const login = async (req, res, next) => {
   try {
@@ -49,16 +48,16 @@ export const login = async (req, res, next) => {
   }
 };
 
-export const logout = async (req, res) => {
-  try {
-    res
-      .clearCookie("accessToken", {
-        sameSite: "none",
-        secure: true,
-      })
-      .status(200)
-      .send("User has been logged out");
-  } catch (err) {
-    next(err);
-  }
-};
+// export const logout = async (req, res) => {
+//   try {
+//     res
+//       .clearCookie("accessToken", {
+//         sameSite: "none",
+//         secure: true,
+//       })
+//       .status(200)
+//       .send("User has been logged out");
+//   } catch (err) {
+//     next(err);
+//   }
+// };
